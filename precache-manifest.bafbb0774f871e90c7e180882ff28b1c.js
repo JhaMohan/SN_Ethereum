cb508b027092529f90903e32a335d41c9b566787
Aditya <adityamohan1994@gmail.com>
2020-06-20 22:12:44 +0530

    Updates

---
+++ precache-manifest.bafbb0774f871e90c7e180882ff28b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fc08cf118a21eae009a",
+    "url": "/SN_Ethereum/static/css/main.4356c566.chunk.css"
+  },
+  {
+    "revision": "9fc08cf118a21eae009a",
+    "url": "/SN_Ethereum/static/js/main.9fc08cf1.chunk.js"
+  },
+  {
+    "revision": "d694139946fc9ce844a6",
+    "url": "/SN_Ethereum/static/css/1.c4ce2113.chunk.css"
+  },
+  {
+    "revision": "d694139946fc9ce844a6",
+    "url": "/SN_Ethereum/static/js/1.d6941399.chunk.js"
+  },
+  {
+    "revision": "ed054daf49216ccc0037",
+    "url": "/SN_Ethereum/static/js/runtime~main.ed054daf.js"
+  },
+  {
+    "revision": "62347c3dbf937b15ba66c93eff35cc68",
+    "url": "/SN_Ethereum/index.html"
+  }
+];
